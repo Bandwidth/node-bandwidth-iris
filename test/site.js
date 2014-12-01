5086d89a2ad90197623a4da1ecdf355141153e7a
Andrey Belchikov <belchikovs@gmail.com>
2014-12-01 00:31:08 -0800

    Completed Site

--- test/site.js
+++ test/site.js
@@ -41,6 +41,16 @@ describe("Site", function(){
         done();
       });
     });
+
+    it("should fail for error status code", function(done){
+      helper.nock().get("/v1.0/accounts/FakeAccountId/sites").reply(400);
+      Site.list(helper.createClient(), function(err, list){
+        if(err){
+          return done();
+        }
+        done(new Error("An error is estimated"));
+      });
+    });
   });
   describe("#get", function(){
     it("should return a site", function(done){
@@ -67,6 +77,15 @@ describe("Site", function(){
         item.name.should.equal("Test Site");
         item.description.should.equal("A Site Description");
         done();
+      });
+    });
+    it("should fail for error status code", function(done){
+      var span = helper.nock().get("/v1.0/accounts/FakeAccountId/sites/1").reply(400);
+      Site.get(helper.createClient(), "1",  function(err, item){
+        if(err){
+          return done();
+        }
+        done(new Error("An error is estimated"));
       });
     });
   });
@@ -99,6 +118,16 @@ describe("Site", function(){
         done();
       });
     });
+    it("should fail on error status code", function(done){
+      var data = {name: "Test Site", description: "A Site Description"};
+      helper.nock().post("/v1.0/accounts/FakeAccountId/sites").reply(400, "");
+      Site.create(helper.createClient(), data,  function(err, item){
+        if(err){
+          return done();
+        }
+        done(new Error("An error is expected"));
+      });
+    });
   });
   describe("#update", function(){
     it("should update a site", function(done){
@@ -117,6 +146,99 @@ describe("Site", function(){
       site.id = 1;
       site.client = helper.createClient();
       site.delete(done);
+    });
+  });
+  describe("#getSipPeers", function(){
+    it("should return list of sippeers", function(done){
+      var span = helper.nock().get("/v1.0/accounts/FakeAccountId/sites/1/sippeers").reply(200, helper.xml.sipPeers, {"Content-Type": "application/xml"});
+      var site = new Site();
+      site.client = helper.createClient();
+      site.id = "1";
+      site.getSipPeers(function(err, list){
+        if(err){
+          return done(err);
+        }
+        span.isDone().should.be.true;
+        list.length.should.equal(1);
+        list[0].id.should.equal(12345);
+        list[0].peerName.should.equal("SIP Peer 1");
+        list[0].description.should.equal("Sip Peer 1 description");
+        done();
+      });
+    });
+    it("should fail on error status code", function(done){
+      helper.nock().get("/v1.0/accounts/FakeAccountId/sites/1/sippeers").reply(400);
+      var site = new Site();
+      site.client = helper.createClient();
+      site.id = "1";
+      site.getSipPeers(function(err, list){
+        if(err){
+          return done();
+        }
+        done(new Error("An error is expected"));
+      });
+    });
+  });
+  describe("#getSipPeer", function(){
+    it("should return a sip peer", function(done){
+      var span = helper.nock().get("/v1.0/accounts/FakeAccountId/sites/1/sippeers/10").reply(200, helper.xml.sipPeer, {"Content-Type": "application/xml"});
+      var site = new Site();
+      site.client = helper.createClient();
+      site.id = "1";
+      site.getSipPeer("10", function(err, item){
+        if(err){
+          return done(err);
+        }
+        span.isDone().should.be.true;
+        item.id.should.equal(10);
+        item.peerName.should.equal("SIP Peer 1");
+        item.description.should.equal("Sip Peer 1 description");
+        done();
+      });
+    });
+    it("should fail on error status code", function(done){
+      helper.nock().get("/v1.0/accounts/FakeAccountId/sites/1/sippeers/10").reply(400);
+      var site = new Site();
+      site.client = helper.createClient();
+      site.id = "1";
+      site.getSipPeer("10", function(err, item){
+        if(err){
+          return done();
+        }
+        done(new Error("An error is expected"));
+      });
+    });
+  });
+  describe("#createSipPeer", function(){
+    it("should create a sip peer", function(done){
+      var data = {peerName: "SIP Peer 1", description: "Sip Peer 1 description"};
+      helper.nock().post("/v1.0/accounts/FakeAccountId/sites/1/sippeers", helper.buildXml({sipPeer: data})).reply(201, "", {"Location": "/v1.0/accounts/FakeAccountId/sites/1/sippeers/10"});
+      helper.nock().get("/v1.0/accounts/FakeAccountId/sites/1/sippeers/10").reply(200, helper.xml.sipPeer, {"Content-Type": "application/xml"});
+      var site = new Site();
+      site.client = helper.createClient();
+      site.id = "1";
+      site.createSipPeer(data, function(err, item){
+        if(err){
+          return done(err);
+        }
+        item.id.should.equal(10);
+        item.peerName.should.equal("SIP Peer 1");
+        item.description.should.equal("Sip Peer 1 description");
+        done();
+      });
+    });
+    it("fail on error status code", function(done){
+      var data = {peerName: "SIP Peer 1", description: "Sip Peer 1 description"};
+      helper.nock().post("/v1.0/accounts/FakeAccountId/sites/1/sippeers").reply(400, "<Response><ErrorCode>400</ErrorCode><Description>Error</Description></Response>");
+      var site = new Site();
+      site.client = helper.createClient();
+      site.id = "1";
+      site.createSipPeer(data, function(err, item){
+        if(err){
+          return done();
+        }
+        done(new Error("An error is expected"));
+      });
     });
   });
 });
